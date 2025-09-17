d2075910cc0e64c6d557a6cd82a28254da8e6fea
Deep Dwivedii <dwivedideepumesh@gmail.com>
2025-09-17 02:42:08 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+
+const firebaseConfig = {
+  "projectId": "studio-8723461064-1f69a",
+  "appId": "1:407435573533:web:b37c85765767fd607d09f4",
+  "storageBucket": "studio-8723461064-1f69a.firebasestorage.app",
+  "apiKey": "AIzaSyA8-Nv0TupsrH9UOHKbkC87pRG9mQI5KMQ",
+  "authDomain": "studio-8723461064-1f69a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "407435573533"
+};
+
+// Initialize Firebase
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { firebaseApp };
